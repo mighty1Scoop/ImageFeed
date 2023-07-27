bd7bf0b3316feb30972ca62b4b341409af2a277d
NIkolay Kozlov <zr4x@ya.ru>
2023-07-27 19:08:22 +0500

    сверстал кодом экран входа

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -7,23 +7,72 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
-
+final class AuthViewController: UIViewController {
+    
+    private let logoImage = UIImageView()
+    private let signInButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUp()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension AuthViewController {
+    private func setUp() {
+        configureView()
+        configureLogoImage()
+        configureButton()
+        configureBackButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        view.backgroundColor = .ypBlack
     }
-    */
-
+    
+    private func configureLogoImage() {
+        view.addSubview(logoImage)
+        
+        let image = UIImage(named: "auth_screen_logo") ?? UIImage()
+        logoImage.image = image
+        
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func configureButton() {
+        view.addSubview(signInButton)
+        
+        signInButton.setTitle("Войти", for: .normal)
+        signInButton.titleLabel?.font = .boldSystemFont(ofSize: 17)
+        signInButton.tintColor = .ypBlack
+        signInButton.backgroundColor = .ypWhite
+        signInButton.layer.cornerRadius = 16
+        signInButton.layer.masksToBounds = true
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            signInButton.heightAnchor.constraint(equalToConstant: 48),
+            signInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            signInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            signInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -90),
+        ])
+        
+        signInButton.addTarget(self, action: #selector(didTappedLogInButton), for: .touchUpInside)
+    }
+    
+    func configureBackButton() {
+        let image = UIImage(named: "nav_back_button_black") ?? UIImage()
+        navigationController?.navigationBar.backIndicatorImage = image
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = image
+        navigationController?.navigationBar.backItem?.title = ""
+    }
+    
+    @objc private func didTappedLogInButton(_ sender: UIButton) {
+        navigationController?.pushViewController(WebViewController(), animated: true)
+    }
 }
+
--- ImageFeed/Auth/WebViewController.swift
+++ ImageFeed/Auth/WebViewController.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController {
+final class WebViewController: UIViewController {
+    
+    private let webView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension WebViewController {
+}
+
